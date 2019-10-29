07ffd8354ca2bef8360b07cb391e347804ba3af8
Atif Abdul Ghafoor <atifabdulghafoor2@gmail.com>
2019-10-29 17:33:33 +0500

    Configured and Extended Devise

---
+++ db/migrate/20191029060442_add_fields_to_users.rb
@@ -0,0 +1,10 @@
+class AddFieldsToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :username, :string
+    add_index :users, :username, unique: true
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+    add_column :users, :birthdate, :Date
+  end
+end
+    
