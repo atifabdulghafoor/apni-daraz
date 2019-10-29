efa2a7034ea70871e1ded10c0b09f39256a2ef13
Atif Abdul Ghafoor <atifabdulghafoor2@gmail.com>
2019-10-29 10:53:21 +0500

    Converted Erb views to slim using erb2slim

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require('webpack')
+
+environment.plugin.append('Provide', new webpack.Provide({
+    $: 'jquery',
+    jQuery: 'jquery',
+    Popper: ['popper.js', 'default'] 
+}))
 
 module.exports = environment
