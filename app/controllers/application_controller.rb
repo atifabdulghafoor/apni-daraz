8c2f7cd4956a25bf2a93be0f43444325b496bd74
Atif Abdul Ghafoor <atifabdulghafoor2@gmail.com>
2019-10-30 13:26:37 +0500

    Formatted Models and Controllers using Rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
-class ApplicationController < ActionController::Base
-    protect_from_forgery with: :exception
+# frozen_string_literal: true
 
-    before_action :configure_permitted_parameters, if: :devise_controller?
-  
-    protected
-  
-      def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:firstname, :lastname, :email, :birthdate, :address, :password])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:firstname, :lastname, :email, :birthdate, :address, :password])
-      end
+# Application Controller
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[firstname lastname \
+                                                         email birthdate \
+                                                         address password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[firstname \
+                                                                lastname email \
+                                                                birthdate \
+                                                                address \
+                                                                password])
+  end
 end
