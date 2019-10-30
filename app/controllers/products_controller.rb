48b2be449f7c81b3c47885086e9cf88d83af3708
Atif Abdul Ghafoor <atifabdulghafoor2@gmail.com>
2019-10-30 18:33:35 +0500

    Created Products Controller

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Products Controller
+class ProductsController < ApplicationController
+end
---
+++ app/helpers/products_helper.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+# Products Helper Module
+module ProductsHelper
+end
